bce053d18845e73b6e8cdc13f244d8b0ff2e60b2
Emilio Sanches <emilio.neto.32123@gmail.com>
2022-05-04 09:02:13 -0300

    Add FeedbackContentStep component
    Create component to render feedback form with screenshot button and textarea

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,4 +1,5 @@
 import { FeedbackType, FEEDBACK_TYPES } from "..";
+import { CloseButton } from "../../CloseButton";
 
 interface FeedbackTypeStepProps {
   onFeedbackTypeSelected: (feedbackType: FeedbackType) => void;
@@ -6,17 +7,23 @@ interface FeedbackTypeStepProps {
 
 export function FeedbackTypeStep({ onFeedbackTypeSelected }: FeedbackTypeStepProps) {
   return (
-    <div className='flex py-8 gap-2 w-full'>
-      {Object.entries(FEEDBACK_TYPES).map(([key, value]) => (
-        <button
-          key={key}
-          className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none'
-          onClick={() => onFeedbackTypeSelected(key as FeedbackType)}
-        >
-          <img src={value.image.source} alt={value.image.alt} />
-          <span >{value.title}</span>
-        </button>
-      ))}
-    </div>
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+      <div className='flex py-8 gap-2 w-full'>
+        {Object.entries(FEEDBACK_TYPES).map(([key, value]) => (
+          <button
+            key={key}
+            className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none'
+            onClick={() => onFeedbackTypeSelected(key as FeedbackType)}
+          >
+            <img src={value.image.source} alt={value.image.alt} />
+            <span>{value.title}</span>
+          </button>
+        ))}
+      </div>
+    </>
   );
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -35,17 +35,18 @@ export type FeedbackType = keyof typeof FEEDBACK_TYPES;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
   return (
     <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeSelected={setFeedbackType} />
       ) : (
-        <FeedbackContentStep />
+        <FeedbackContentStep
+        feedbackType={feedbackType}
+        onRestartFeedbackRequested={handleRestartFeedback} />
       )}
 
       <footer>Feito com ♥ por <a className="underline underline-offset-2" href="https://linked.in/emiliosanches">Emilio Sanches</a></footer>
