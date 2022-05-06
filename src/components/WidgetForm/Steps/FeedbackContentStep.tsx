210b719fea0e6f6c2c3d2dc35714fe489390917e
Emilio Sanches <emilio.neto.32123@gmail.com>
2022-05-06 08:36:57 -0300

    Integrate front-end with back-end

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { useState } from "react";
 import { FeedbackType, FEEDBACK_TYPES } from "..";
+import { api } from "../../../libs/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -13,13 +15,22 @@ interface FeedbackContentStepProps {
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = FEEDBACK_TYPES[feedbackType]
 
-  function handleSubmitFeedback(e: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmitFeedback(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    console.log(screenshot)
-    console.log(comment)
+
+    setIsSendingFeedback(true)
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot
+    })
+
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -56,10 +67,14 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
 
           <button
             type='submit'
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500'
           >
-            Enviar feedback
+            {
+              isSendingFeedback ? (
+                <Loading />
+              ) : 'Enviar feedback'
+            }
           </button>
         </footer>
       </form>
